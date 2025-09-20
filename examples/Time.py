afdff6c37df30a51d5b8fae33f73c178e10871ca
Beta-Verse-Hub <baibhavkumarjha1@gmail.com>
2025-09-20 20:06:53 +0530

    bug fix

--- examples/Time.py
+++ examples/Time.py
@@ -1,12 +1,13 @@
 # Import required modules
 import PyConText
+import keyboard
 import time
 
 # Clear the console
 PyConText.Console.clear()
 
 # Loop until the escape key is pressed
-while not PyConText.keyboard.is_pressed("esc"):
+while not keyboard.is_pressed("esc"):
     PyConText.Cursor.move(0, 0) # Move the cursor to the top left
     PyConText.Widget.output(f"Current time: {time.strftime('%H:%M:%S')}", alignment="center") # Output the current time
-    time.sleep(1) # Wait for 1 second
+    time.sleep(0.01) # Wait for 1 second
